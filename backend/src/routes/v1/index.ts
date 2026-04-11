4eee162c62a85c74b78772e0f2e786df2fc06568
sahir <sahirchawla2004@gmail.com>
2026-04-11 15:13:43 +0530

    BE-feat: integrating task routs in v1

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { authRouter } from "./auth.routes"
 import { projectRouter } from "./project.routes"
+import { taskRouter } from "./task.routes"
 
 export const v1Router = Router()
 
 v1Router.use("/auth", authRouter)
 v1Router.use("/projects", projectRouter)
+v1Router.use("/tasks", taskRouter)
