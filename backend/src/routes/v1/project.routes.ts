e0cb180db9ff98889a6fb9245d76f09eb6d26ce4
sahir <sahirchawla2004@gmail.com>
2026-04-11 15:15:41 +0530

    BE-feat: adding routes for project tasks

--- backend/src/routes/v1/project.routes.ts
+++ backend/src/routes/v1/project.routes.ts
@@ -6,7 +6,9 @@ import {
   listProjectsController,
   updateProjectController,
 } from "../../controllers/project.controller"
+import { createTaskController, listProjectTasksController } from "../../controllers/task.controller"
 import { authMiddleware } from "../../middlewares/auth.middleware"
+import { projectAccessMiddleware } from "../../middlewares/project.middleware"
 
 export const projectRouter = Router()
 
@@ -17,3 +19,14 @@ projectRouter.post("/", createProjectController)
 projectRouter.get("/:id", getProjectByIdController)
 projectRouter.patch("/:id", updateProjectController)
 projectRouter.delete("/:id", deleteProjectController)
+
+projectRouter.get(
+  "/:projectId/tasks",
+  projectAccessMiddleware,
+  listProjectTasksController
+)
+projectRouter.post(
+  "/:projectId/tasks",
+  projectAccessMiddleware,
+  createTaskController
+)
