35dcc79952b8096a8d3d94dace76bef28ded97ee
sahir <sahirchawla2004@gmail.com>
2026-04-11 15:09:33 +0530

    feat: using the common util for response in auth midware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,18 +1,19 @@
-import {Request, Response, NextFunction} from "express"
-import { verifyJwtToken } from "../utils/jwt"
+import { Request, Response, NextFunction } from "express"
 import { jwtPayloadSchema } from "../schema/auth.schema"
+import { verifyJwtToken } from "../utils/jwt"
+import { errorResponse } from "../utils/response"
 
-export const authMiddleware = async (req:Request, res:Response, next:NextFunction) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    if(!token){
-        return res.status(401).json({ error: "unauthorized" })
-    }
-    try{
-        const decoded = verifyJwtToken(token);
-        const payload = jwtPayloadSchema.parse(decoded);
-        req.user = payload;
-        return next();
-    }catch(error){
-        return res.status(401).json({ error: "unauthorized" })
-    }
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1]
+  if (!token) {
+    return res.status(401).json(errorResponse("unauthorized"))
+  }
+  try {
+    const decoded = verifyJwtToken(token)
+    const payload = jwtPayloadSchema.parse(decoded)
+    req.user = payload
+    return next()
+  } catch {
+    return res.status(401).json(errorResponse("unauthorized"))
+  }
 }
