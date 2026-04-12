71c19dbc26a12c6b09921b9e22aa6ad17872332f
sahir <sahirchawla2004@gmail.com>
2026-04-12 23:50:24 +0530

    remove build artifacts from version control

--- backend/dist/config/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtSecret = exports.clients = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.clients = process.env.CLIENTS?.split(",") || [];
-exports.jwtSecret = process.env.JWT_SECRET;
-//# sourceMappingURL=env.js.map
--- backend/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-app_1.app.listen(8000, () => {
-    console.log("Server is running on port 8000");
-});
-//# sourceMappingURL=index.js.map
